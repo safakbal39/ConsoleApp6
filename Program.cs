abfc0d0dfd720d015c359a61aad8692ba29d990a
monut <monut@DESKTOP-75A4Q55>
2024-12-07 17:31:08 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+//var not = 60;
+Console.WriteLine("Not gir");
+int not = int.Parse(Console.ReadLine());
+
+if(not>50) {
+    Console.WriteLine("GEÇTİN");
+}
+if (not <= 50)
+{
+    Console.WriteLine("Kaldiniz");
+}
